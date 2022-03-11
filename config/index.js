bd309e276d1c97c283f9cf0c4a1e8f734ac284a9
Eliza <seeubanks12@gmail.com>
2022-03-11 00:41:47 -0500

    done

--- config/index.js
+++ config/index.js
@@ -30,19 +30,19 @@ module.exports = (app) => {
 
   // controls a very specific header to pass headers from the frontend
   // ! please configure the cors `origin` key so that you can accept the requests wherever they might be coming from
-  app.use(
-    cors({
-      credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
-    })
-  );
-  // console.log(process.env.FRONTEND_URL);
   // app.use(
   //   cors({
   //     credentials: true,
-  //     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  //     origin: process.env.ORIGIN || "http://localhost:3000",
   //   })
   // );
+  console.log(process.env.FRONTEND_URL);
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
